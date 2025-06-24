b4f4fb965d0429699768582dc2ec966531d797a4
kelvin okumu <105194367+kelvinokumu@users.noreply.github.com>
2025-06-24 22:32:25 +0300

    added postregsql and requirements

--- demo_django/settings.py
+++ demo_django/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = "demo_django.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_django',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
